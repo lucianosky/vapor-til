6e506e0878618407fb560ff3e9f22934880c13a9
Luciano Sclovsky <luciano.sclovsky@softdesign-rs.com.br>
2018-08-29 18:46:53 -0300

    Chapter 11: Added tests

--- Tests/AppTests/UserTests.swift
+++ Tests/AppTests/UserTests.swift
@@ -27,6 +27,7 @@ final class UserTests: XCTestCase {
             username: usersUsername,
             on: conn)
         _ = try User.create(on: conn)
+
         let users = try app.getResponse(
             to: usersURI,
             decodeTo: [User].self)
@@ -35,5 +36,76 @@ final class UserTests: XCTestCase {
         XCTAssertEqual(users[0].username, usersUsername)
         XCTAssertEqual(users[0].id, user.id)
     }
+
+    func testUserCanBeSavedWithAPI() throws {
+        let user = User(name: usersName, username: usersUsername)
+        let receivedUser = try app.getResponse(
+            to: usersURI,
+            method: .POST,
+            headers: ["Content-Type": "application/json"],
+            data: user,
+            decodeTo: User.self)
+
+        XCTAssertEqual(receivedUser.name, usersName)
+        XCTAssertEqual(receivedUser.username, usersUsername)
+        XCTAssertNotNil(receivedUser.id)
+
+        let users = try app.getResponse(
+            to: usersURI,
+            decodeTo: [User].self)
+
+        XCTAssertEqual(users.count, 1)
+        XCTAssertEqual(users[0].name, usersName)
+        XCTAssertEqual(users[0].username, usersUsername)
+        XCTAssertEqual(users[0].id, receivedUser.id)
+    }
+
+    func testGettingASingleUserFromTheAPI() throws {
+        let user = try User.create(
+            name: usersName,
+            username: usersUsername,
+            on: conn)
+        let receivedUser = try app.getResponse(
+            to: "\(usersURI)\(user.id!)",
+            decodeTo: User.self)
+
+        XCTAssertEqual(receivedUser.name, usersName)
+        XCTAssertEqual(receivedUser.username, usersUsername)
+        XCTAssertEqual(receivedUser.id, user.id)
+    }
     
+    func testGettingAUsersAcronymsFromTheAPI() throws {
+        let user = try User.create(on: conn)
+        let acronymShort = "OMG"
+        let acronymLong = "Oh My God"
+
+        let acronym1 = try Acronym.create(
+            short: acronymShort,
+            long: acronymLong,
+            user: user,
+            on: conn)
+        _ = try Acronym.create(
+            short: "LOL",
+            long: "Laugh Out Loud",
+            user: user,
+            on: conn)
+
+        let acronyms = try app.getResponse(
+            to: "\(usersURI)\(user.id!)/acronyms",
+            decodeTo: [Acronym].self)
+
+        XCTAssertEqual(acronyms.count, 2)
+        XCTAssertEqual(acronyms[0].id, acronym1.id)
+        XCTAssertEqual(acronyms[0].short, acronymShort)
+        XCTAssertEqual(acronyms[0].long, acronymLong)
+    }
+    
+    static let allTests = [
+        ("testUsersCanBeRetrievedFromAPI", testUsersCanBeRetrievedFromAPI),
+        ("testUserCanBeSavedWithAPI", testUserCanBeSavedWithAPI),
+        ("testGettingASingleUserFromTheAPI", testGettingASingleUserFromTheAPI),
+        ("testGettingAUsersAcronymsFromTheAPI", testGettingAUsersAcronymsFromTheAPI)
+    ]
+
 }
+
